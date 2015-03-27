eeb38f8ef34e45c9e904b14246af5d4ffa703554
Tom Mertz <tom.mertz@verizon.net>
2015-03-27 09:30:28 -0700

    Renamed Shoe class to Brand

---
+++ src/Brand.php
@@ -0,0 +1,15 @@
+<?php
+
+    class Brand
+    {
+        private $id;
+        private $name;
+
+        function __construct($new_name, $new_id = null)
+        {
+            $this->name = $new_name;
+            $this->id = $new_id;
+        }
+    }
+
+ ?>
