3e965a598b4ede5444bc5510dae7833b4381d50e
Tom Mertz <tom.mertz@verizon.net>
2015-03-27 13:27:24 -0700

    Add silex and twig vendor files

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit1cc03b3d12559a0712f8d44d15f52c71::getLoader();
+return ComposerAutoloaderInit4ffd1e60a57105d0d8e2bdebd498a599::getLoader();
